52e11034873b750081f51afce75067df3289a7d2
Lucas Andión Montáns <andion@gmail.com>
2010-09-29 23:58:15 -0700

    Adapted jcropper to work with Paperclip::Thumbnail after this commit: http://github.com/thoughtbot/paperclip/commit/724cc7e1a2b035b2f9c35c8a17adfc5383746deb
    Original source: http://railscasts.com/episodes/182-cropping-images#comment_106608

--- lib/paperclip_processors/jcropper.rb
+++ lib/paperclip_processors/jcropper.rb
@@ -9,7 +9,7 @@ module Paperclip
     
     def transformation_command
       if crop_command
-        crop_command + super.sub(/ -crop \S+/, '')
+        crop_command + super.join(' ').sub(/ -crop \S+/, '').split(' ')
       else
         super
       end
@@ -18,7 +18,7 @@ module Paperclip
     def crop_command
       target = @attachment.instance
       if target.cropping?
-        " -crop '#{target.crop_w.to_i}x#{target.crop_h.to_i}+#{target.crop_x.to_i}+#{target.crop_y.to_i}'"
+        ["-crop","#{target.crop_w.to_i}x#{target.crop_h.to_i}+#{target.crop_x.to_i}+#{target.crop_y.to_i}"]
       end
     end
   
